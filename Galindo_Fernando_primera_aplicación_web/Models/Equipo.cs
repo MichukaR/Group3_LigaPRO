bdc7e1c3c24cc2114eaad3ac97b76dcf8e5f52cd
Michu <michelle.reyes.delgado@udla.edu.ec>
2025-04-30 22:21:18 -0500

    Primer commit

--- Galindo_Fernando_primera_aplicación_web/Models/Equipo.cs
+++ Galindo_Fernando_primera_aplicación_web/Models/Equipo.cs
@@ -8,13 +8,18 @@ namespace Galindo_Fernando_primera_aplicación_web.Models
         public int Id { get; set; }
         [Required]
         [MaxLength(100)]
+        [Display(Name = "Nombre del Equipo")]
         public string Nombre { get; set; }
         [Range(0,20)]
+        [Display(Name = "Ingrese la cantidad de Partidos Jugados")]
         public int PartidosJugados {  get; set; }
+        [Display(Name = "Partidos Ganados")]
         [Range(0, 20)]
         public int PartidosGanados { get; set; }
+        [Display(Name = "Partidos Empatados")]
         [Range(0, 20)]
         public int PartidosEmpatados { get; set; }
+        [Display(Name = "Partidos Perdidos")]
         [Range(0, 20)]
         public int PartidosPerdidos { get; set; }
         public int TotalPuntos {
